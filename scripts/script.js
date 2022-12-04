84b2e5dcad9258e8698a62f76028e498b0ed8fc5
valeroman <valeraromanovo@gmail.com>
2022-12-04 19:53:52 +0300

    Create buttons

--- scripts/script.js
+++ scripts/script.js
@@ -20,6 +20,10 @@ const controlPanelDiv = document.createElement('div');
 controlPanelDiv.setAttribute('id', 'controlPanelDiv');
 calculatorDiv.appendChild(controlPanelDiv);
 
+const controlsPad = document.createElement('div');
+controlsPad.setAttribute('id', 'controlsPad');
+controlPanelDiv.appendChild(controlsPad);
+
 const numPad = document.createElement('div');
 numPad.setAttribute('id', 'numPad');
 controlPanelDiv.appendChild(numPad);
@@ -27,3 +31,20 @@ controlPanelDiv.appendChild(numPad);
 const operPad = document.createElement('div');
 operPad.setAttribute('id', 'operPad');
 controlPanelDiv.appendChild(operPad);
+
+const controlsArray = ['AC', '+/-', '%'];
+const numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '.'];
+const operArray = ['+', '-', '*', '/', '='];
+
+addButtonsToPad(controlsArray, controlsPad, 'controlsBtn');
+addButtonsToPad(numArray, numPad, 'numBtn');
+addButtonsToPad(operArray, operPad, 'operBtn');
+
+function addButtonsToPad(buttonsArr, pad, className) {
+    buttonsArr.forEach(btnContent => {
+        let btn = document.createElement('button');
+        btn.setAttribute('class', className);
+        btn.textContent = btnContent;
+        pad.appendChild(btn);
+    });
+}
