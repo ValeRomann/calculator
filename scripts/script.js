09470cb1cc2360eaab6ab1a50d9d9b6cdc40b9c2
valeroman <valeraromanovo@gmail.com>
2022-12-04 17:22:26 +0300

    Create display divs

--- scripts/script.js
+++ scripts/script.js
@@ -8,6 +8,14 @@ const displayDiv = document.createElement('div');
 displayDiv.setAttribute('id', 'displayDiv');
 calculatorDiv.appendChild(displayDiv);
 
+const currentValueDisplay = document.createElement('div');
+currentValueDisplay.setAttribute('id', 'currentValue');
+displayDiv.appendChild(currentValueDisplay);
+
+const resultValueDisplay = document.createElement('div');
+resultValueDisplay.setAttribute('id', 'resultValue');
+displayDiv.appendChild(resultValueDisplay);
+
 const controlPanelDiv = document.createElement('div');
 controlPanelDiv.setAttribute('id', 'controlPanelDiv');
 calculatorDiv.appendChild(controlPanelDiv);
