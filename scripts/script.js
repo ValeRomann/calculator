d90e98ff7ee9439b157fab4de851df49c673cb42
valeroman <valeraromanovo@gmail.com>
2022-12-04 20:32:51 +0300

    Arrange buttons

--- scripts/script.js
+++ scripts/script.js
@@ -20,6 +20,10 @@ const controlPanelDiv = document.createElement('div');
 controlPanelDiv.setAttribute('id', 'controlPanelDiv');
 calculatorDiv.appendChild(controlPanelDiv);
 
+const operPad = document.createElement('div');
+operPad.setAttribute('id', 'operPad');
+controlPanelDiv.appendChild(operPad);
+
 const controlsPad = document.createElement('div');
 controlsPad.setAttribute('id', 'controlsPad');
 controlPanelDiv.appendChild(controlsPad);
@@ -27,10 +31,6 @@ controlPanelDiv.appendChild(controlsPad);
 const numPad = document.createElement('div');
 numPad.setAttribute('id', 'numPad');
 controlPanelDiv.appendChild(numPad);
-
-const operPad = document.createElement('div');
-operPad.setAttribute('id', 'operPad');
-controlPanelDiv.appendChild(operPad);
 
 const controlsArray = ['AC', '±', '%'];
 const numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '.'];
